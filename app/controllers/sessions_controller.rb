a4ad36143c0fc79739d8ff5b80499ad3ca9944fa
travisbrault <brault.travis@gmail.com>
2013-06-18 11:13:01 -0400

    compile assets

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,9 +8,10 @@ class SessionsController < ApplicationController
   		if user && user.authenticate(params[:session][:password])
     		sign_in user
     		redirect_to user 
-	else
-    	flash.now[:error] = 'Invalid email/password combination'
-    	render 'new'
+		else
+	    	flash.now[:error] = 'Invalid email/password combination'
+	    	render 'new'
+	    end
 	end
 
 	def destroy
