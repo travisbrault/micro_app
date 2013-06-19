9901137c71e55800f36d9aab9382b94093327a13
travisbrault <brault.travis@gmail.com>
2013-06-19 14:29:40 -0400

    add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,20 @@ class UsersController < ApplicationController
 	before_filter :correct_user,   only: [:edit, :update]
 	before_filter :admin_user,     only: :destroy
 
+	def following
+		@title = "Following"
+		@user = User.find(params[:id])
+		@users = @user.followed_users.paginate(page: params[:page])
+		render 'show_follow'
+	end
+
+	def followers
+		@title = "Followers"
+		@user = User.find(params[:id])
+		@users = @user.followers.paginate(page: params[:page])
+		render 'show_follow'
+	end
+
 	def show
 		@user = User.find(params[:id])
 		@microposts = @user.microposts.paginate(page: params[:page])
--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,15 @@ MicroApp::Application.routes.draw do
     match '/about', to: 'static_pages#about'
     match '/contact', to: 'static_pages#contact'
 
-    resources :users
-    resources :sessions,   only: [:new, :create, :destroy]
-    resources :microposts, only: [:create, :destroy]
+    resources :users do
+      member do
+        get :following, :followers
+      end
+    end
+  
+    resources :sessions,      only: [:new, :create, :destroy]
+    resources :microposts,    only: [:create, :destroy]
+    resources :relationships, only: [:create, :destroy]
 
     match '/signup', to: 'users#new'
     match '/signin', to: 'sessions#new'
